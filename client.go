c36857a3bfac5f2412d309865fa14d5b8936ddbd
Boran Ozdeger <boranozdeger@Borans-MacBook-Pro.local>
2022-10-21 16:05:58 +0100

    Manuel testing

--- client.go
+++ client.go
@@ -3,24 +3,49 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net"
+	"os"
+	"strings"
+	"sync"
 )
 
-type client struct {
-	conn     net.Conn
-	username string
+func monitorSocket(conn net.Conn, wg sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		status, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println("Unable to read input from the server ", err.Error())
+		}
+		status = strings.Trim(status, "\r\n")
+		fmt.Println(status)
+	}
+}
+
+func sendMessage(conn net.Conn, wg sync.WaitGroup) {
+	fmt.Println("input text:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = conn.Write([]byte(scanner.Text()))
 }
 
 func main() {
+	wg := sync.WaitGroup{}
 	fmt.Println("Client Starting...")
 	conn, err := net.Dial("tcp", ":8080")
+
 	if err != nil {
 		fmt.Println("Unable to connect to server: ", err.Error())
 	}
 
-	status, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		fmt.Println("Unable to read input from the server ", err.Error())
-	}
-	fmt.Println(status)
+	wg.Add(1)
+	go monitorSocket(conn, wg)
+	//go sendMessage(conn, wg)
+
+	wg.Wait()
 }
--- server.go
+++ server.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"sync"
 )
 
 const (
 	PORT string = ":8080"
 )
 
+type client struct {
+	conn     net.Conn
+	username string
+}
+
+var clients []*client
+
 func newClient(conn net.Conn) {
 	fmt.Println("Client connected: ", conn.RemoteAddr().String())
 
-	var cli *client
-	cli.conn = conn
-	cli.username = "anonymous"
+	cli := &client{
+		conn:     conn,
+		username: "Anonymous",
+	}
 
+	clients = append(clients, cli)
 	fmt.Println(cli.username)
 }
 
+func handleUserConnection(conn net.Conn, wg sync.WaitGroup) {
+
+	// userInput, err := bufio.NewReader(conn).ReadString('\n')
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// fmt.Println(userInput)
+	_, e := clients[0].conn.Write([]byte("Hello!" + "\n"))
+
+	if e != nil {
+		log.Fatalln("unable to write over client connection")
+	}
+}
+
 func main() {
+	wg := sync.WaitGroup{}
 	fmt.Println("Server starting...")
 	ln, err := net.Listen("tcp", PORT)
 
@@ -28,12 +54,15 @@ func main() {
 	}
 
 	defer ln.Close()
-	fmt.Println("Server sterted on port ", PORT)
+	fmt.Println("Server started on port ", PORT)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println("Unable to connect to client: ", err.Error())
 		}
-		go newClient(conn)
+		newClient(conn)
+		handleUserConnection(conn, wg)
+
+		wg.Wait()
 	}
 }
